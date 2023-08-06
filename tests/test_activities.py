45f27bbc19745ec676f30af3d129a9512c2822bf
Gian Klug <gian.klug@ict-scouts.ch>
2023-08-06 22:52:33 +0200

    chore: Ignore ruff assert error for tests

--- tests/test_activities.py
+++ tests/test_activities.py
@@ -1,13 +1,13 @@
-import pytest
 from click.testing import CliRunner
+
 from timedctl.timedctl import timedctl
+
 
 def test_current_activity():
     runner = CliRunner()
-    result = runner.invoke(timedctl, ['get', 'reports'])
+    result = runner.invoke(timedctl, ["get", "reports"])
     assert result.exit_code == 0
-    assert 'Reports for today:' in result.output
-    result = runner.invoke(timedctl, ['get', 'reports', '--date', '2000-01-01'])
+    assert "Reports for today:" in result.output
+    result = runner.invoke(timedctl, ["get", "reports", "--date", "2000-01-01"])
     assert result.exit_code == 0
-    assert 'Reports for 2000-01-01:' in result.output
-
+    assert "Reports for 2000-01-01:" in result.output
